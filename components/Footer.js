529cfca94a60516bf7ffa88126e41a8cd9667ace
Daniel Bogod <daniel.bogod@historicengland.org.uk>
2022-03-04 10:42:42 +0000

    Add Ko-fi link to footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,34 +1,47 @@
 ﻿import styles from '../styles/Footer.module.scss';
-import { BsTwitter, BsGithub } from "react-icons/bs";
+import { BsTwitter, BsGithub } from 'react-icons/bs';
+import { BiCoffeeTogo } from 'react-icons/bi';
 
-const Footer = ({isSettingsOpen}) => {
+const Footer = ({ isSettingsOpen }) => {
   return (
     <>
-      <footer className={`${styles.footer} ${isSettingsOpen ? 'position-absolute bottom-0': ''} w-100`}>
+      <footer className={`${styles.footer} ${isSettingsOpen ? 'position-absolute bottom-0' : ''} w-100`}>
         <div className="container">
           <div className={`${styles.footerContent} py-1`}>
 
-              <a
-                href="https://twitter.com/bogod"
-                aria-labelledby="twitter-link-desc">
+            <a
+              href="https://twitter.com/bogod"
+              aria-labelledby="twitter-link-desc">
                 <span
                   id="twitter-link-desc"
                   className="visually-hidden">
                 Follow me on Twitter
               </span>
-                <BsTwitter className={styles.iconLink}/>
-              </a>
+              <BsTwitter className={styles.iconLink}/>
+            </a>
             &nbsp; | &nbsp;
-              <a
-                href="https://github.com/dbogod/wordle-monkey"
-                aria-labelledby="github-link-desc">
+            <a
+              href="https://github.com/dbogod/wordle-monkey"
+              aria-labelledby="github-link-desc">
                 <span
                   id="github-link-desc"
                   className="visually-hidden">
                   Take a look at the source code on Github
                 </span>
-                <BsGithub className={styles.iconLink}/>
-              </a>
+              <BsGithub className={styles.iconLink}/>
+            </a>
+            &nbsp; | &nbsp;
+            <a
+              href="https://ko-fi.com/bogod"
+              aria-labelledby="github-link-desc"
+              title="Buy me a coffee!">
+                <span
+                  id="github-link-desc"
+                  className="visually-hidden">
+                  Like the monkey? Buy the organ grinder a coffee!
+                </span>
+              <BiCoffeeTogo className={styles.iconLink}/>
+            </a>
           </div>
         </div>
       </footer>
